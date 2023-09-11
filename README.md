3864231e22ee2efa5c1e58038978c46b67d2eb7b
thl <tanghailong@daoba.com>
2023-09-11 17:17:06 +0800

    'init'

---
+++ README.md
@@ -0,0 +1,14 @@
+# 使用步骤
+
+## 安装vant
+
+官方地址https://vant-contrib.gitee.io/vant-weapp/#/quickstart
+
+```js
+# 通过 npm 安装
+npm i @vant/weapp -S --production
+```
+
+## 构建npm
+
+微信开发者工具=>工具=>构建npm
---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    active: 0,
+  },
+  onChange(event) {
+    // event.detail 的值为当前选中项的索引
+    this.setData({ active: event.detail });
+  },
+  onLoad() {
+
+  },
+
+})
